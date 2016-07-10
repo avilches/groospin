64259e74fbf00b44309aa97132be30ce9b759dae
Alberto Vilches <vilches@gmail.com>
2016-07-11 00:30:51 +0200

    check script only for singles checks

--- src/main/groovy/Check.groovy
+++ src/main/groovy/Check.groovy
@@ -6,13 +6,7 @@ HyperSpin hs = new HyperSpin(
         "D:/Games/HyperSpin-fe",
         "D:/Games/RocketLauncher")
 
-new Checker(hs).
-        addHandler(new HumanInfo()).
-        addHandler(new HaveHtmlList("D:/Games/Soft/GrooSpin/report/all.html", true)).
-        addHandler(new HaveHtmlList("D:/Games/Soft/GrooSpin/report/have-list.html", false)).
-        addHandler(new MissingTxtList("D:/Games/Soft/GrooSpin/report/missing.csv", ";")).
-        addHandler(new AllRomsCsvList("D:/Games/Soft/GrooSpin/report/roms.csv", ";")).
-        addHandler(new SystemCsvList("D:/Games/Soft/GrooSpin/report/systems.csv", ";")).
-        checkSystems()
+new Checker(hs).addHandler(new HumanInfo()).checkSystem("Amstrad CPC")
+
 
 
