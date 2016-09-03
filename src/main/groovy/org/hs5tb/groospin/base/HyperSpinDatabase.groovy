f491006f015542549104722a37cb676270828670
Alberto Vilches <vilches@gmail.com>
2016-09-03 02:08:28 +0200

    genres, letters and cloneof bug fixed

--- src/main/groovy/org/hs5tb/groospin/base/HyperSpinDatabase.groovy
+++ src/main/groovy/org/hs5tb/groospin/base/HyperSpinDatabase.groovy
@@ -1,6 +1,7 @@
 package org.hs5tb.groospin.base
 
 import groovy.xml.MarkupBuilder
+import org.hs5tb.groospin.common.StringHelper
 
 /**
  * Created by Alberto on 01-Sep-16.
@@ -12,6 +13,37 @@ class HyperSpinDatabase {
     String listversion
     String exporterversion
     List<Rom> roms
+
+    static void writeGenres(List<Rom> roms, String folder) {
+        writeGenres(roms, new File(folder))
+    }
+
+    static void writeGenres(List<Rom> roms, File folder) {
+        writeGenres(roms.findAll { it.genre }.groupBy { it.genre }, folder)
+    }
+
+    static void writeGenres(Map<String, List<Rom>> romsByGenre, String folder) {
+        writeGenres(romsByGenre, new File(folder))
+    }
+
+    static void writeGenres(Map<String, List<Rom>> romsByGenre, File folder) {
+        if (!romsByGenre) return
+        StringBuffer genreContent = new StringBuffer()
+        romsByGenre.keySet().sort().each {
+            genreContent << "  <game name=\"${it}\"/>\n"
+        }
+        new File(folder, "genre.xml").text = "<menu>\n${genreContent}</menu>"
+
+        romsByGenre.each { String genre, List<MameMachine> genreRoms ->
+            write(genreRoms, new File(folder, "${StringHelper.unescapeHTML(genre)}.xml"))
+        }
+    }
+
+    static void fixDatabase(File db) {
+        HyperSpinDatabase hyperSpinDatabase = new HyperSpinDatabase()
+        hyperSpinDatabase.load(db)
+        hyperSpinDatabase.export(db)
+    }
 
     HyperSpinDatabase load(File db, Closure filter = null) {
         Node menu = new XmlParser().parse(db.newReader())
@@ -55,24 +87,23 @@ class HyperSpinDatabase {
 
     static void write(List<Rom> roms, Writer writer, Map headers = [:]) {
         StringBuilder lettersAlreadyUsed = new StringBuilder()
-        new MarkupBuilder(writer).menu {
-            setOmitEmptyAttributes(true)
+        MarkupBuilder mb = new MarkupBuilder(writer)
+        mb.mkp.xmlDeclaration(version: "1.0", encoding: "utf-8")
+        mb.menu {
+            setOmitNullAttributes(true)
+            setDoubleQuotes(true)
             header {
                 if (headers.listname) listname(headers.listname)
                 if (headers.listversion) listversion(headers.listversion)
                 if (headers.lastlistupdate) lastlistupdate(headers.lastlistupdate)
                 if (headers.exporterversion) exporterversion(headers.exporterversion)
             }
-            roms.each { Rom rom ->
-                String image = indexImage(rom.name.toUpperCase().charAt(0))
-                if (lettersAlreadyUsed.contains(image)) {
-                    image = null // Si ya estaba la letra, no la usamos
-                } else {
-                    lettersAlreadyUsed.append(image) // la marcamos como añadida para la proxima
-                }
-                game(name: rom.name, index: image ? "true" : "", image: image?.toLowerCase()) {
+            roms.sort { it.description }.each { Rom rom ->
+                String image = getLetter(lettersAlreadyUsed, rom)
+                game(name: rom.name, index: image ? "true" : "", image: image?.toLowerCase() ?: "") {
                     description(rom.description)
-                    if (rom.cloneof) cloneof(rom.cloneof)
+                    cloneof(rom.cloneof) // clone of is mandatory, even if it's empty! HS fails if you use parents_only=true and there is no <cloneof> tag!
+                    if (rom.crc) crc(rom.crc)
                     if (rom.manufacturer) manufacturer(rom.manufacturer)
                     if (rom.year) year(rom.year)
                     if (rom.genre) genre(rom.genre)
@@ -84,7 +115,22 @@ class HyperSpinDatabase {
         }
     }
 
-    static private String indexImage(char letter) {
+    private static String getLetter(StringBuilder lettersAlreadyUsed, Rom rom) {
+        String image
+        if (!rom.cloneof) {
+            // Only the parents have letter because if you use the parents_only=true flag and
+            // choose a letter which belongs to a clone, HS will jump to the beginning
+            image = fixLetter(rom.description.toUpperCase().charAt(0))
+            if (lettersAlreadyUsed.contains(image)) {
+                image = null // Si ya estaba la letra, no la usamos
+            } else {
+                lettersAlreadyUsed << image // la marcamos como añadida para la proxima
+            }
+        }
+        return image
+    }
+
+    static private String fixLetter(char letter) {
         String image = letter
         if (letter < (char) '0') {
             image = '0'
