e34f166c318292b99503fcda39af53b69d6e9c32
Alberto Vilches <vilches@gmail.com>
2016-10-26 22:21:25 +0200

    default settings for hyperspin

--- src/main/groovy/examples/SettingsChange.groovy
+++ src/main/groovy/examples/SettingsChange.groovy
@@ -14,7 +14,26 @@ HyperSpin hs = new HyperSpin("D:/Games/RocketLauncher")
 // hs.changeHyperSpinSettings("AAE", "exe info", "hola", "que tal4")
 
 hs.withHyperSpinAllSystemSettings { String filename, IniFile ini ->
-    println "${filename}: ${ini.get("themes", "use_parent_vids")}"
 
+    ini.put("navigation", "use_last_game", "true")
+    ini.put("navigation", "random_game", "false")
 
+    ini.put("themes", "use_parent_vids", "true")
+    ini.put("themes", "use_parent_themes", "true")
+
+    ini.put("filters", "parents_only", "false")
+    ini.put("filters", "themes_only", "false")
+    ini.put("filters", "wheels_only", "false")
+    ini.put("filters", "roms_only", "false")
+
+    if (ini.dirty) ini.store()
+
+//    println "${filename}: ${ini.get("navigation", "use_last_game")}"
+//    println "${filename}: ${ini.get("navigation", "random_game")}"
+//    println "${filename}: ${ini.get("themes", "use_parent_vids")}"
+
+//    println "${filename}: ${ini.get("filters", "parents_only")}"
+//    println "${filename}: ${ini.get("filters", "themes_only")}"
+//    println "${filename}: ${ini.get("filters", "wheels_only")}"
+//    println "${filename}: ${ini.get("filters", "roms_only")}"
 }
