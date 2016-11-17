cfbb5a6a7cd0283caadf210e86f4ee4c8eabf0d8
Alberto Vilches <vilches@gmail.com>
2016-11-17 15:56:02 +0100

    refactor

--- src/main/groovy/examples/mame/FillMediaFromCombined.groovy
+++ src/main/groovy/examples/mame/FillMediaFromCombined.groovy
@@ -3,6 +3,7 @@ package examples.mame
 import operation.RomMediaOperations
 import org.hs5tb.groospin.base.HyperSpin
 import org.hs5tb.groospin.base.RLSystem
+import org.hs5tb.groospin.base.Rom
 import org.hs5tb.groospin.checker.BaseCheckHandler
 import org.hs5tb.groospin.checker.Checker
 import org.hs5tb.groospin.checker.result.CheckRomResult
@@ -13,16 +14,15 @@ HyperSpin hs = new HyperSpin("D:/Games/RocketLauncher")
 // Se supone que antes se ha ejecutado CombineMediaMame y tenemos en MAME todos los medias mezclados de todos
 // los sistemas
 
-def systems = (hs.listSystems().findAll { it.defaultEmulator.name.startsWith("MAME") }*.name) - "MAME"
-RLSystem mame = hs.getSystem("MAME")
+def systems = ["SNK Neo Geo AES"] + ((hs.listSystems().findAll { it.defaultEmulator?.name?.startsWith("MAME") }*.name) - "MAME")
+RLSystem mameSystem = hs.getSystem("MAME")
 RomMediaOperations romMediaOperations = new RomMediaOperations(hs)
 romMediaOperations.simulation = false
-new Checker(hs).
-        addHandler(new BaseCheckHandler() {
-            @Override
-            void romChecked(CheckRomResult checkRomResult) {
-                romMediaOperations.copyMedia(checkRomResult.romName, mame, checkRomResult.system)
-            }
 
-        }).
-        checkSystems(systems)
+systems.each { String s ->
+    RLSystem systemTo = hs.getSystem(s)
+    systemTo.listRoms().each { Rom rom ->
+        romMediaOperations.copyMedia(rom.name, mameSystem, systemTo)
+    }
+
+}
