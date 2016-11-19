1cc1d69e6c119e63d797e35540810e5b86785c8b
Alberto Vilches <vilches@gmail.com>
2016-11-19 17:47:18 +0100

    settings

--- src/main/groovy/examples/SettingsChange.groovy
+++ src/main/groovy/examples/SettingsChange.groovy
@@ -24,16 +24,16 @@ hs.withRocketLauncherInis(hs.listSystemNames().collect { "Settings/$it/RocketLau
 
 hs.withHyperSpinAllSystemSettings { String filename, IniFile ini ->
 
-//    ini.put("navigation", "use_last_game", "true")
-//    ini.put("navigation", "random_game", "false")
+    ini.put("navigation", "use_last_game", "true")
+    ini.put("navigation", "random_game", "false")
 //
-//    ini.put("themes", "use_parent_vids", "true")
-//    ini.put("themes", "use_parent_themes", "true")
+    ini.put("themes", "use_parent_vids", "true")
+    ini.put("themes", "use_parent_themes", "true")
 //
-//    ini.put("filters", "parents_only", "false")
-//    ini.put("filters", "themes_only", "false")
-//    ini.put("filters", "wheels_only", "false")
-//    ini.put("filters", "roms_only", "false")
+    ini.put("filters", "parents_only", "false")
+    ini.put("filters", "themes_only", "false")
+    ini.put("filters", "wheels_only", "false")
+    ini.put("filters", "roms_only", "false")
 
     if (ini.dirty) ini.store()
 
