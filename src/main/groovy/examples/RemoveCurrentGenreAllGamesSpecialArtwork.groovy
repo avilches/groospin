340227c59cd5e6fb3f13ca32d2246e4687b81df8
Alberto Vilches <vilches@gmail.com>
2016-11-20 19:53:02 +0100

    fix special art c

--- src/main/groovy/examples/RemoveCurrentGenreAllGamesSpecialArtwork.groovy
+++ src/main/groovy/examples/RemoveCurrentGenreAllGamesSpecialArtwork.groovy
@@ -1,6 +1,7 @@
 package examples
 
 import org.hs5tb.groospin.base.HyperSpin
+import org.hs5tb.groospin.common.IniFile
 
 /**
  * Created by Alberto on 20-Nov-16.
@@ -11,6 +12,15 @@ HyperSpin hs = new HyperSpin("D:/Games/RocketLauncher")
 hs.listSystems().each {
     File toDelete = it.newMediaPath("Images/Special/All Games.png")
     if (toDelete.exists()) {
-        println toDelete
+        toDelete.delete()
+        println "$toDelete deleted"
+    }
+}
+
+hs.withHyperSpinAllSystemSettings { String filename, IniFile ini ->
+    ini.put("Special Art C", "active", "true")
+    if (ini.dirty) {
+        println "Special Art C activated in $filename"
+        ini.store()
     }
 }
