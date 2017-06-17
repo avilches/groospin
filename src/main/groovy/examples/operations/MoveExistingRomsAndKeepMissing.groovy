f99489a2ec82a163ffb9443d6ae05aa5688b40a4
hs5tb <hyperspin5tb@gmail.com>
2017-06-17 09:40:23 +0200

    comments

--- src/main/groovy/examples/operations/MoveExistingRomsAndKeepMissing.groovy
+++ src/main/groovy/examples/operations/MoveExistingRomsAndKeepMissing.groovy
@@ -4,11 +4,24 @@ import operation.Operations
 import operation.RomFileOperations
 import org.hs5tb.groospin.base.HyperSpin
 
-String dst = "d:\\Games\\Isos\\Sega Saturn Japan\\out"
-List systems = ["Sega Saturn Japan"]
+List systems = ["Nintendo Entertainment System"]
 
 RomFileOperations operations = new RomFileOperations(new HyperSpin("D:/Games/RocketLauncher"))
 operations.simulation = false
-operations.moveRomsTo(dst, [Operations.EXISTS], systems)
+
+/*
+Mueve las roms correctas a una carpeta especifica, dejando
+las incorrectas en su carpeta original
+*/
+
+// String dst = "d:\\Games\\Isos\\Sega Saturn Japan\\out"
+// operations.moveRomsTo(dst, [Operations.EXISTS], systems)
+
+/*
+Les añade una extension nueva (sin borrar la extensión original) a las roms correctas, dejando
+las incorrectas con su extensión original
+*/
+
+// operations.addSuffixToRomName(".ok", [Operations.EXISTS], systems)
 
 
