ef1249f50793794e5b4ab2cf5678cda821a864ce
hs5tb <hyperspin5tb@gmail.com>
2017-08-02 03:22:58 +0200

    packer

--- src/main/groovy/operation/Packer.groovy
+++ src/main/groovy/operation/Packer.groovy
@@ -2,6 +2,7 @@ package operation
 
 import org.hs5tb.groospin.base.HyperSpin
 import org.hs5tb.groospin.base.RLSystem
+import org.hs5tb.groospin.checker.handlers.HumanInfo
 import org.hs5tb.groospin.common.IOTools
 
 /**
@@ -42,13 +43,17 @@ class Packer extends Operations {
             rar = rar+".rar"
         }
         String txt = ""
-        combineAndOptimizeResourceList(resources).each { File originFile ->
+        resources = combineAndOptimizeResourceList(resources)
+        listFiles(resources, rar - ".rar")
+
+        resources.each { File originFile ->
             txt += "${relativeToRoot(originFile)}\n"
         }
         File f = File.createTempFile("groospin-packer-rar", "lst")
         try {
             f.text = txt
             String cmd = "${RAR_EXE} a -r0 -v500000 \"${new File(rar).absolutePath}\" @${f.absolutePath}"
+            println "---------------------------------------------------------------------"
             println "cwd $root"
             println cmd
             if (!simulation) {
@@ -58,31 +63,40 @@ class Packer extends Operations {
         } finally {
             f.delete()
         }
-        listFiles(resources, rar - ".rar")
     }
 
     void listFiles(List resources, String file) {
         Set<File> files = explodeToSingleFileList(resources)
-        new File(file+".txt").text = files.collect{ relativeToRoot(it) }.join("\n")
+        new File(file+".txt").text = files.collect{
+            relativeToRoot(it)
+        }.join("\n")
         new File(file+".csv").text = "size,content\n${files.collect{ "${it.file?it.size():0},${relativeToRoot(it)}" }.join("\n")}\n${files.sum { it.file?it.size():0}}"
     }
 
     Set<File> explodeToSingleFileList(List resources) {
         Set<File> files = new HashSet()
-        combineAndOptimizeResourceList(resources).each { File originFile ->
+        long total = 0
+        resources.each { File originFile ->
             if (originFile.directory) {
+                long dirsize = 0
                 originFile.eachFileRecurse { File contentFile ->
                     // Se incluyen también los directorios
+                    dirsize += contentFile.size()
                     files << contentFile
                 }
+                println "${IOTools.humanReadableByteSize(dirsize).padLeft(10, " ")} ${originFile.absolutePath}"
+                total += dirsize
             } else {
                 files << originFile
+                println "${IOTools.humanReadableByteSize(originFile.size()).padLeft(10, " ")} ${originFile.absolutePath}"
+                total += originFile.size()
             }
         }
+        println "${IOTools.humanReadableByteSize(total)}"
         return files.sort { it.absolutePath.toLowerCase()}
     }
 
-    List<String> combineAndOptimizeResourceList(List resources) {
+    List<File> combineAndOptimizeResourceList(List resources) {
         List dirs = []
         List files = []
         resources.each { o ->
@@ -91,37 +105,45 @@ class Packer extends Operations {
                 dirs << originFile
             } else if (originFile.file) {
                 files << originFile
+            } else {
+                println "[-] Not found ${originFile}"
             }
         }
-        List bestDirs = []
+        List<File> bestDirs = []
         dirs.sort{ it.absolutePath.toLowerCase() }.each { File current ->
-            if (!bestDirs.find { File other -> current.absolutePath.toLowerCase().startsWith(other.absolutePath.toLowerCase())}) {
+            File include = bestDirs.find { File other -> current.absolutePath.toLowerCase().startsWith(other.absolutePath.toLowerCase() + "\\") }
+            if (!include) {
                 bestDirs << current
+            } else {
+                println "[!] Ignoring folder ${current.absolutePath} (included in ${include.absolutePath})"
             }
         }
-        List filesNotIncludedInDirs = []
+        List<File> filesNotIncludedInDirs = []
         files.sort{ it.absolutePath.toLowerCase() }.each { File current ->
-            if (!bestDirs.find { File other -> current.absolutePath.toLowerCase().startsWith(other.absolutePath.toLowerCase())}) {
+            File include = bestDirs.find { File other -> current.absolutePath.toLowerCase().startsWith(other.absolutePath.toLowerCase())}
+            if (!include) {
                 filesNotIncludedInDirs << current
+            } else {
+                println "Ignoring file ${current.absolutePath} (included in ${include.absolutePath})"
             }
         }
+        println "* FILES *"
+        println filesNotIncludedInDirs.join("\n")
+        println "* FOLDERS *"
+        println bestDirs.join("\n")
         return bestDirs + filesNotIncludedInDirs
     }
 
-    List listSystemResources(List systems) {
-        systems.collect { listSystemResources(it) }.flatten()
-    }
-
-    List listSystemResources(String systemName) {
-        RLSystem system = hyperSpin.getSystem(systemName)
+    List listSystemResources(RLSystem system) {
         File mainMenu = hyperSpin.newHyperSpinMediaFile("Main Menu")
         [system.getMediaFolder(),
-         new File(mainMenu, "Images/Wheel/${systemName}.png"),
-         new File(mainMenu, "Themes/${systemName}.zip"),
-         new File(mainMenu, "Video/${systemName}.mp4"),
-         new File(mainMenu, "Video/${systemName}.flv"),
-         hyperSpin.newHyperSpinFile("Settings/${systemName}.ini"),
-         hyperSpin.newHyperSpinFile("Databases/${systemName}")
-        ]
+         new File(mainMenu, "Images/Wheel/${system.name}.png"),
+         new File(mainMenu, "Themes/${system.name}.zip"),
+         new File(mainMenu, "Video/${system.name}.mp4"),
+         new File(mainMenu, "Video/${system.name}.flv"),
+         hyperSpin.newHyperSpinFile("Settings/${system.name}.ini"),
+         hyperSpin.newHyperSpinFile("Databases/${system.name}")
+        ] + system.romPathsList
+
     }
 }
