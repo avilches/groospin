645583d0a620844f8dcbf60de1bf4a499f1ee946
hs5tb <hyperspin5tb@gmail.com>
2017-08-27 23:37:39 +0200

    my map drive

--- src/main/groovy/examples/MapDrive.groovy
+++ src/main/groovy/examples/MapDrive.groovy
@@ -9,8 +9,9 @@ import org.hs5tb.groospin.base.HyperSpin
 
 HyperSpin hs = new HyperSpin("D:\\Games\\RocketLauncher")
 
-//new ConfigJoyToKeyXbox360(hs).execute(new File("D:\\parches\\mapping\\360"))
+new ConfigJoyToKeyXbox360(hs).execute() // new File("D:\\parches\\mapping\\360-nuevo"))
 //new ConfigJoyToKeyNone(hs).execute(new File("D:\\parches\\mapping\\reset"))
-new ConfigJoyToKeyArcade(hs).execute(new File("D:\\parches\\mapping\\AT"),
-        new ArcadeSetAT())
+//new ConfigJoyToKeyArcade(hs).execute(new ArcadeSetAT(), new File("D:\\parches\\mapping\\AT"))
 
+//HyperSpin hs = new HyperSpin("A:\\RocketLauncher")
+//new ConfigJoyToKeyArcade(hs).execute(new ArcadeSetAT(), new File("A:\\parches\\mapping\\AT"))
